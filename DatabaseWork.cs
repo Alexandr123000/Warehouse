5f5e5dd5dbce4bc119356f41023252dc294bee34
Alexandr123000 <alexandrt10000@gmail.com>
2026-03-13 16:17:26 +0300

    feat: add databasework class

---
+++ DatabaseWork.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.IO;
+using System.Text;
+
+namespace Warehouse
+{
+    public class DatabaseWork
+    {
+        public static string currentDirectory = Directory.GetCurrentDirectory();
+        public static string changedDirectory = System.IO.Path.GetFullPath(System.IO.Path.Combine(currentDirectory, @"../../../"));
+        public static string databaseFile = @"Data Source=" + changedDirectory + "Database.db";
+        SQLiteConnection connection = new SQLiteConnection(databaseFile);
+        public SQLiteCommand command = new SQLiteCommand();
+        public DatabaseWork()
+        {
+            connection.Open();
+            command.Connection = connection;
+            command.CommandText = @"CREATE TABLE sells (id INTEGER PRIMARY KEY AUTOINCREMENT, amount INTEGER, txt TEXT);";
+            command.ExecuteNonQuery();
+            for (int i = 0; i < 10; i++)
+            {
+                command.CommandText = "INSERT INTO sells (amount, txt) VALUES ('" + Convert.ToString(i) + "', 'LLL');";
+                command.ExecuteNonQuery();
+            }
+            command.CommandText = @"SELECT * FROM sells";
+            var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                MessageBox.Show(Convert.ToString(reader.GetInt64(0)) + Convert.ToString(reader.GetInt64(1)) + reader.GetString(2));
+            }
+            connection.Close();
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,39 +13,11 @@ using System.IO;
 
 namespace Warehouse
 {
-    
-
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
         }
-        public static string currentDirectory = Directory.GetCurrentDirectory();
-        public static string changedDirectory = System.IO.Path.GetFullPath(System.IO.Path.Combine(currentDirectory, @"../../../"));
-        public static string databaseFile = @"Data Source=" + changedDirectory + "---";
-        SQLiteConnection connection = new SQLiteConnection(databaseFile);
-        public SQLiteCommand command = new SQLiteCommand();
-        
-        connection.Open();
-
-        command.Connection = connection;
-        command.CommandText = @"CREATE TABLE sells (id INTEGER PRIMARY KEY AUTOINCREMENT, amount INT)";
-        command.ExecuteNonQuery();
-        for (int = 1; i < 20; i++)
-        {
-            command.CommandText = "INSERT INTO sells (amount) VALUES (" + Convert.ToString(i+3) + ")";
-            command.ExecuteNonQuery();
-        }
-        /*
-        command.CommandText = "SELECT * FROM sells"
-        var reader = command.Execute.Reader();
-        while (reader.Read())
-        {
-            MainLabel.Content = reader.GetString(1);
-        }*/
-
-
-
     }
 }
